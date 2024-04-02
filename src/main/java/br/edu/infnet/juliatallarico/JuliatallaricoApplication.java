bc7f205d843770d0785a2c65327aecc14a3c8761
Júlia <julia.tallcaroline@outlook.com>
2024-04-01 22:50:47 -0300

    pull com a main

--- src/main/java/br/edu/infnet/juliatallarico/JuliatallaricoApplication.java
+++ src/main/java/br/edu/infnet/juliatallarico/JuliatallaricoApplication.java
@@ -3,6 +3,8 @@ package br.edu.infnet.juliatallarico;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class JuliatallaricoApplication {
 
